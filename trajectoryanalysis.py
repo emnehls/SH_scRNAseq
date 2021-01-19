8d20ed60f7cd720c1ac67f24646b1aad6a931c38
emnehls_home <emnehls@protonmail.com>
2021-01-19 13:45:08 -0600

    Added a chunk that imports scVelo through my python engine. This won't work on any other computer as written...

---
+++ trajectoryanalysis.py
@@ -0,0 +1,14 @@
+## A script to maybe run the trajectory analysis on naive.integrated.
+
+import scvelo as scv
+
+adata = scv.read("files/naive.integrated.h5ad")
+print(adata)
+
+
+scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
+scv.pp.moments(adata, n_pcs=30, n_neighbors=30)
+scv.tl.velocity(adata)
+scv.tl.velocity_graph(adata)
+scv.pl.velocity_embedding_stream(adata, basis="umap", color="seurat_clusters")
+
